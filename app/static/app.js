ddb25e48720ac01d3d00391e213945f7f3851b36
qubard <qubard@gmail.com>
2019-03-09 03:39:46 -0500

    tooltip with total sim time

--- app/static/app.js
+++ app/static/app.js
@@ -6,6 +6,20 @@ app.config(function($interpolateProvider) {
     $interpolateProvider.endSymbol('@@');
 });
 
+app.filter('dateFilter', ['$filter', function($filter) {
+    return function(input) {
+        d = input;
+        var h = Math.floor(d / 3600);
+        var m = Math.floor(d % 3600 / 60);
+        var s = Math.floor(d % 3600 % 60);
+
+        var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+        var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+        var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+        return hDisplay + mDisplay + sDisplay;
+    }
+}]);
+
 app.controller('MainCtrl', function($scope, $http, $timeout) {
     var loadTime = 5000,
     errorCount = 0, // Counter for the server errors
