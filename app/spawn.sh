18e0e543f75a324982c2f3d49794a61b72edc91d
cub <qubard@gmail.com>
2019-10-28 15:12:11 -0400

    fixed bug in spawn script
    using cert instead of fullchain as certificate file

--- app/spawn.sh
+++ app/spawn.sh
@@ -1,2 +1,2 @@
-gunicorn -w 4 -b 0.0.0.0:443 app:app --certfile=fullchain.pem --keyfile=privkey.pem --daemon
+gunicorn -w 4 -b 0.0.0.0:443 app:app --certfile=cert.pem --keyfile=privkey.pem --daemon
 gunicorn -b 0.0.0.0:80 app:app --daemon
